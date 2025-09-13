1509cedac71d86d71e5f18b6109f654d66b6eaeb
leminhthanh9823 <thanhlmhe176868@fpt.edu.vn>
2025-09-13 13:38:03 +0700

    init commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const mongoose = require("mongoose");
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Express API 🚀" });
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error(err));
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
