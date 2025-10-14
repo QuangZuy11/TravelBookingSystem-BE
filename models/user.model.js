810d3ae2e7522217bae3b572964c8b5dedd60d36
truongbabiboi1204 <Caovantruong2503@gmail.com>
2025-10-14 21:31:14 +0700

    update profile

--- models/user.model.js
+++ models/user.model.js
@@ -1,39 +1,36 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: mongoose.Schema.Types.ObjectId, ref: "Role", required: true },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-  },
-  gender: {
-    type: String,
-    enum: ['Nam', 'Nữ', 'Khác'],
-    default: 'Khác',
-  },
-  date_of_birth: {
-    type: Date,
-  },
-  city: {
-    type: String,
-  },
-  role: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Role',
-    required: true,
-  },
-}, { timestamps: true });
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (_, ret) => {
+        delete ret.password;
+        return ret;
+      },
+    },
+    toObject: {
+      virtuals: true,
+      transform: (_, ret) => {
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
+);
+
+userSchema.virtual("traveler", {
+  ref: "Traveler",
+  localField: "_id",
+  foreignField: "user_id",
+  justOne: true,
+});
 
 module.exports = mongoose.model('User', userSchema);
